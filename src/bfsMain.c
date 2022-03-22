cfc8a0e39550fca2b9eb5afeca365845b531d2c2
budzynsf <filipbudzynski18@gmmail.com>
2022-03-22 10:39:27 +0100

    male zmiany w bfsMain

--- src/bfsMain.c
+++ src/bfsMain.c
@@ -6,10 +6,19 @@
 #define MAX_BUF 1024
 
 int main(int argc, char** argv){
+  if(argc < 3) {
+    printf("Nie podano węzła rozpoczynającego\n");
+    return 1;
+  }
   int w, k;
   char *nazwa_pliku = argv[1];
   FILE *in = fopen(argv[1], "r");
+  struct Graph* graf = readGraph(in);
   int start = atoi(argv[2]);
+  if(start >= graf->k*graf->w){
+    printf("Podano numer węzła który nie należy do grafu\n");
+    return 1;
+  }
 
   FILE *out = stdout;
 
